cec9b1f486fc4029ef847f3add9186fa3e755966
JMC <lovestory0617@gmail.com>
2026-09-21 15:26:01 +0900

    feat: 스프링 프로젝트 초기 세팅 완료

---
+++ src/main/java/com/aicamp/dxproject/DxprojectApplication.java
@@ -0,0 +1,13 @@
+package com.aicamp.dxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aicamp/dxproject/DxprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicamp.dxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
